fa92639e9d848b2f7b3c0138453e1e306a39c0ce
Arnav-Sangle <90949554+Arnav-Sangle@users.noreply.github.com>
2023-05-18 16:40:09 +0530

    it works

--- greedy.cpp
+++ greedy.cpp
@@ -5,34 +5,57 @@ using namespace std;
 
 int main()
 {
-    int arr[10] = {100,10,50,50,25,25,20,10,10,5};
+    int arr[10] = {100,100,50,50,25,25,20,10,10,5};
     int select[10];
     int capacity=175;
-    int visited[10];
+    int notSelect[10];
+    int notVisited[10];
     
-    int a=0; 
+    //selected 
+    //not selected
+    //remaining not visited         neither select nor notSelect
+    
+    int sum=0; 
+    int a=0;
     int b=0;
-    int k=0;
+    int c=0;
+    int remaining = capacity;
     for(int i=0;i<10;i++) {
-        if(capacity>=a+arr[i]) {
-            a=a+arr[i];
-            capacity=capacity-arr[i];
-            select[i]=arr[i];
-            k++;
+        if(capacity>=sum+arr[i]) {
+            sum=sum+arr[i];
+            select[a]=arr[i];
+            a++;
+            remaining = remaining-arr[i];
+        }
+        else if(remaining==0) {
+            notVisited[c] = arr[i];
+            c++;
         }
         else {
-            a = a-arr[i];
+            notSelect[b] = arr[i];
+            b++;
         }
     }
     
     
     
-    cout<<"Selected"<<endl;
-    for(int i=0; i<k; i++) {
-        if(select[i]>0) {
-            cout<<select[i]<<endl;
-        }
+    cout<<"Selected: ";
+    for(int i=0; i<a; i++) {
+        cout<<select[i]<<" ";
     }
+    cout<<endl;
+    
+    cout<<"Not Selected: ";
+    for(int i=0; i<b; i++) {
+        cout<<notSelect[i]<<" ";
+    }
+    cout<<endl;
+    
+    cout<<"Not Visited: ";
+    for(int i=0; i<c; i++) {
+        cout<<notVisited[i]<<" ";
+    }
+    cout<<endl;
     
     return 0;
 }
