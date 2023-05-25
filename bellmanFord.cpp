ce4c956fde6c3f8758669258aadef71670bbb04b
Arnav-Sangle <90949554+Arnav-Sangle@users.noreply.github.com>
2023-05-25 16:52:15 +0530

    initial try, just started bellman ford algo

---
+++ bellmanFord.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+    void bellmanFord(vector<vector<int>> graph, int distance[]) {
+        for (int i : graph) {
+            
+        }
+    }
+
+int main() {
+    
+    vector<vector<int>> graph = { {INF, 4, 2, INF, INF},
+                            {INF, INF, 3, 2, 4},
+                            {INF, 1, 2, INF, 4, 5},
+                            {INF, INF, INF, INF, INF},
+                            {INF, INF, INF, INF, -5}
+                        }
+     vector<int> distance = {INF, INF, INF, INF, INF};
+    
+    
+    for()
+}
