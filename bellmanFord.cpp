15e3cfced54704a0ad66f3ffebbfc3ab87e5ef33
Arnav-Sangle <90949554+Arnav-Sangle@users.noreply.github.com>
2023-05-26 16:59:46 +0530

    trying ma'am's method

--- bellmanFord.cpp
+++ bellmanFord.cpp
@@ -2,22 +2,38 @@
 #include <iostream>
 
 using namespace std;
-    void bellmanFord(vector<vector<int>> graph, int distance[]) {
-        for (int i : graph) {
-            
-        }
-    }
+
+// void bellmanFord(vector<vector<int>> graph, int distance[]) {
+//     for (int i : graph) {
+        
+//     }
+// }
+
+typedef struct Edge {
+    int u;  //source node
+    int v; //destination node
+    int w; //weight of edge, cost of dist btwn u & v
+} Edge;
+
+typedef struct Graph {
+    int V;  //no. of vertexes
+    int E;  //no. of edges
+    struct Edge *e;  //
+} Graph;
+
 
 int main() {
-    
-    vector<vector<int>> graph = { {INF, 4, 2, INF, INF},
-                            {INF, INF, 3, 2, 4},
-                            {INF, 1, 2, INF, 4, 5},
-                            {INF, INF, INF, INF, INF},
-                            {INF, INF, INF, INF, -5}
-                        }
-     vector<int> distance = {INF, INF, INF, INF, INF};
-    
-    
-    for()
+    Graph *g;   //struct Graph *g;
+    g = new Graph;
+    cout << g;
+       
+    // vector<vector<int>> graph = { {INF, 4, 2, INF, INF},
+    //                         {INF, INF, 3, 2, 4},
+    //                         {INF, 1, 2, INF, 4, 5},
+    //                         {INF, INF, INF, INF, INF},
+    //                         {INF, INF, INF, INF, -5}
+    //                     }
+    //  vector<int> distance = {INF, INF, INF, INF, INF};
 }
+
+
